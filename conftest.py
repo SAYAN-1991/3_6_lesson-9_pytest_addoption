07dfe1ffd0a3b21a3ff07f070fe67f88faa354d4
SAYASHKA1991 <SAYASHKA1991@gmail.com>
2022-06-30 14:44:03 +0800

    * добавление файлов в Git conftest.py, test_items.py.

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="None",
+                     help="Choose language: en-gb, ru, fr, de, es, it, sk, pt")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    if user_language in ("ru", "en", "es"):
+        print("\nstart chrome browser for test language = " + user_language)
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--language should be en, ru, es")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+#pytest --language=es test_items.py
+
---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_basket(browser):
+    browser.get(link)
+    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    time.sleep(15)
